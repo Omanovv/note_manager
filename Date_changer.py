23a4d5d5faa40b7825266f7368bbbb9a03657d45
Omanovv <omanovv@gmail.com>
2025-01-27 17:21:05 +0400

    Add files via upload

---
+++ Date_changer.py
@@ -0,0 +1,11 @@
+created_date = '19-03-2024'
+issue_date = '24-03-2024'
+temp_created_date1 = (created_date[0:2])
+temp_created_date2 = (created_date[3:5])
+temp_issue_date1 = (issue_date[0:2])
+temp_issue_date2 = (issue_date[3:5])
+
+print (temp_created_date1, temp_created_date2, sep='-')
+print (temp_issue_date1, temp_issue_date2, sep='-')
+
+
