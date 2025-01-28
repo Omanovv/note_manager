2fb1424664662ee06f6634a5460d80bdd96539ab
Omanovv <omanovv@gmail.com>
2025-01-29 00:10:31 +0400

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,7 @@
+
+title1 = input('Введите любимый продукт в магазине: ')
+title2 = input('Введите ближайший магазин: ')
+title3 = input('Введите дешёвый магазин: ')
+
+all_title = [title1.title(), title2.title(), title3.title()]
+print (all_title)
